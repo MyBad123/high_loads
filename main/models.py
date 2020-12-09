23f13afe21a12a6d02cee87bba598f357c3a504e
MyBad123 <gena.kyznetsov10@gmail.com>
2020-12-09 23:56:40 +0300

    3 commit

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class MyCity(models.Model):
+    city = models.CharField(max_length=200, primary_key=True)
+
+class MyComplects(models.Model):
+    name = models.CharField(max_length=200, primary_key=True)
+    price = models.IntegerField()
+    about = models.TextField()
+    city = models.ForeignKey('MyCity', on_delete=models.CASCADE)
+
+
