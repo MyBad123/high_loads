22ff7663b89368e072dbf0aebdaa2936d3d7a310
MyBad1231 <gena.kyznetsov10@gmail.com>
2023-04-29 14:01:32 +0300

    29.04: new project(2023)

--- main/routers.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import routers
-#from .views import SnippetViewSet
-
-router = routers.DefaultRouter()
-router.register('snippets', SnippetViewSet)
--- source/urls.py
+++ source/urls.py
@@ -1,7 +1,8 @@
-"""kino URL Configuration
+"""
+URL configuration for source project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,16 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-from django.conf.urls.static import static
-from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('main.urls'))
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)     
